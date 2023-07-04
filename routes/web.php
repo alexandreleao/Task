0bda7685c753d546ee45aa4c1612eb895675e9b6
alexandreleao <erdleosalgueiro@gmail.com>
2023-07-04 17:08:02 -0300

    api crud task list and logout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LogoutController;
+use Illuminate\Support\Facades\Route; 
 
 
 /*
@@ -21,4 +22,5 @@ Route::get('/', function () {
 
 Route::prefix('auth')->group(function(){
     Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class);
 });
