5716508d7dabcfe880a1c7301f2529d6a8bb0e0e
alexandreleao <erdleosalgueiro@gmail.com>
2023-07-05 11:08:39 -0300

    Protected routes

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::prefix('v1')->group(function(){
+Route::middleware('auth:sanctum')->prefix('v1')->group(function(){
+
     Route::apiResource('/tasks', TaskController::class);
+    
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 
 });
